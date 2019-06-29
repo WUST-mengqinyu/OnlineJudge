6b64cdd4dacbb0ff19ccc874c03a68ac80e5a4df
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-06-29 20:11:32 +0800

    增加contest内rejudge

--- submission/views/admin.py
+++ submission/views/admin.py
@@ -20,3 +20,40 @@ class SubmissionRejudgeAPI(APIView):
 
         judge_task.delay(submission.id, submission.problem.id)
         return self.success()
+
+
+class ProblemRejudgeAPI(APIView):
+    @super_admin_required
+    def get(self, request):
+        problem_id = request.GET.get("problem_id")
+        if not problem_id:
+            return self.error("Parameter error, id is required")
+        try:
+            # 增加contest内rejudge
+            submission = Submission.objects.select_related("problem").get(id=id)
+            # submission = Submission.objects.select_related("problem").get(id=id, contest_id__isnull = True)
+        except Submission.DoesNotExist:
+            return self.error("Submission does not exists")
+        submission.statistic_info = {}
+        submission.save()
+
+        judge_task.delay(submission.id, submission.problem.id)
+        return self.success()
+
+
+# class ContestProblemRejudgeAPI(APIView):
+#     @super_admin_required
+#     def get(self, request):
+#         problem_id = request.GET.get("problem_id")
+#         contest_id = request.GET.get("contest_id")
+#         if (not problem_id) or (not contest_id):
+#             return self.error("Parameter error, id is required")
+#         try:
+#             submission = Submission.objects.select_related("problem").get(id=id, contest=contest_id)
+#         except Submission.DoesNotExist:
+#             return self.error("Submission does not exists")
+#         submission.statistic_info = {}
+#         submission.save()
+#
+#         judge_task.delay(submission.id, submission.problem.id)
+#         return self.success()
